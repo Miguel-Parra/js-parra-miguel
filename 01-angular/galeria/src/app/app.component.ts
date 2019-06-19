8ca21d5f35e85bc90d8afa76b7793786ebc29aef
Miguel-Parra <miguelts644@hotmail.com>
2019-06-18 23:28:54 -0500

    Directivas ngIf, ngFor, propertyBinding

--- 01-angular/galeria/src/app/app.component.ts
+++ 01-angular/galeria/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { StringifyOptions } from 'querystring';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Floreria';
+  claseTitulo= 'rojo';
+  estaMostrando='falso';
+
+  arregloFlores = [
+    new Flor("Papitas","A lo bestia"),
+    new Flor("Carnitas","Gorditas"),
+    new Flor("Chicas","Cheveres")
+  ];
+
+
+  arregloFloresJs =[
+    {
+      nombre:"Papitas",
+      description:"A lo bestia",
+      titulo:"Don Jose"
+    },
+    {
+      nombre:"Carnitas",
+      description:"Gorditas",
+      titulo:"Don Pepito"
+
+    },
+    {
+      nombre:"Chicas",
+      description:"Cheveres",
+      titulo:"Doña Maria"
+
+    }
+  ]
+  cambioChela(evento:boolean){
+    //logica para hacerle verde
+    console.log("llego a chela", evento)
+    this.claseTitulo = 'verde'
+  }
+
+  cambioCerveza(evento:boolean){
+//logica para hacerle amarillo
+
+console.log("llego a cerveza", evento)
+this.claseTitulo = 'amarillo'
+}
+
+mostrar(estaMostrando){
+  this.estaMostrando=estaMostrando;
+}
+
+}
+
+
+
+
+
+class Flor{
+  constructor(
+   public  nombre:string, //yo voy a tener una variable en la clase
+   //y que sea publica y se llame nombre y que se llame
+   //construrctor.
+    public descripcion:string
+  ){
+
+  }
 }
--- 01-angular/galeria/src/app/item-galeria/item-galeria.component.ts
+++ 01-angular/galeria/src/app/item-galeria/item-galeria.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-item-galeria',
   templateUrl: './item-galeria.component.html',
   styleUrls: ['./item-galeria.component.css']
 })
-export class ItemGaleriaComponent implements OnInit {
+export class ItemGaleriaComponent implements OnInit, OnDestroy {
  
   title='Licoreria';
 
@@ -14,14 +14,53 @@ export class ItemGaleriaComponent implements OnInit {
 
   @Input()
   textoBoton;
+
+  @Input()
+  titulo;
   
+  //eventos para hijo a papa
+  @Output()
+  cambioChela: EventEmitter<boolean> = new EventEmitter
+
+  @Output()
+  cambioCerveza: EventEmitter<boolean> = new EventEmitter
+
+
+  url="https://http2.mlstatic.com/biela-repuestos-mercedes-benzom-166-D_NQ_NP_642014-MLU26705950558_012018-Q.jpg"
+
+  notas=[1,2,3,4,5,6,7,8,9,10]
   constructor() { }
 
-  ngOnInit() {
-  }
+ 
 
   alertar(){
     alert('Auxilio me desmayo'+this.nombreItem)
+  }
+
+  alertarBlur(){
+    alert('ALERTAR BLUR')
+  }
+
+  cambiarImagen(){
+    const cervezas = "https://eldiariony.files.wordpress.com/2016/10/cervezacata.jpg?quality=80&strip=all&w=693"
+    const chelas = "https://http2.mlstatic.com/biela-repuestos-mercedes-benzom-166-D_NQ_NP_642014-MLU26705950558_012018-Q.jpg"
+    // let url3 = "https://eldiariony.files.wordpress.com/2016/10/cervezacata.jpg?quality=80&strip=all&w=693"
+  if(this.url==cervezas){
+    this.url=chelas
+    this.cambioChela.emit(true)
+  }else{
+    this.url=cervezas
+    this.cambioCerveza.emit(true)
+  }
+  
+  }
+
+  ngOnInit() {
+    console.log('Empezo');
+  }
+
+  ngOnDestroy(){
+    console.log('Termino');
   }
 
 }
